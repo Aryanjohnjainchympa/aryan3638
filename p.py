08acd45013c7f0c5687c18d2dfde6b7a0704d118
Aryanjohnjainchympa <150122375+Aryanjohnjainchympa@users.noreply.github.com>
2024-04-01 16:18:43 +0530

    Add files via upload

---
+++ p.py
@@ -0,0 +1,18 @@
+tuple=('aryan','marian','barbarian')
+print(tuple)
+
+#dict={'fruit':'apple','car':'bugati'}
+#print(dict)
+ 
+# Getting current date and time using now().
+ 
+# importing datetime module for now()
+import datetime
+ 
+# using now() to get current time
+current_time = datetime.datetime.now()
+ 
+# Printing value of now.
+print("Time now at india is:", current_time)
+
+
